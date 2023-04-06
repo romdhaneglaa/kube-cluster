85ac0f5b187e0e077147231de6b0fb4ca4f2feda
romdhane <rglaa@bigapps.fr>
2023-04-06 15:03:29 +0200

    add helm chart

--- helm.tf
+++ helm.tf
@@ -10,7 +10,22 @@ provider "kubernetes" {
   host = data.aws_eks_cluster.default.endpoint
 
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.default.token
+}
 
-
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.default.token
+  }
 
 }
+resource "helm_release" "app-2048" {
+  chart = "${path.module}/helm/2048"
+  name  = "app-2048-helm"
+}
+
+
+
+
