535bba1686de24d0b0cdd66e3cac62e666393179
romdhane <rglaa@bigapps.fr>
2023-05-02 16:52:59 +0200

    access application through https

--- providers.tf
+++ providers.tf
@@ -4,11 +4,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+
+    kustomization = {
+      source  = "kbst/kustomize"
+      version = "0.2.0-beta.3"
+
+    }
+
   }
 
   required_version = ">= 1.2.0"
 }
 
+
+provider "kustomization" {}
+
+
 provider "aws" {
   region = "us-east-1"
 }
+
